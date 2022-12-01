57b1a034c47f2366b67b0da73a0d7fcbebea887a
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2022-12-01 15:30:39 -0700

    Login Assignment Done

--- WPF/Login.xaml.cs
+++ WPF/Login.xaml.cs
@@ -25,5 +25,56 @@ namespace WPF
         {
             InitializeComponent();
         }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt;
+            Hashtable ht = new Hashtable();
+            string sql;
+            txtReturn.Text = "";
+
+            ht.Clear();
+
+            ht.Add("@Username", txtUsername.Text);
+            ht.Add("@Password", txtPassword.Text);
+
+            sql = "Select Name from Users where Username=@Username and Password=@Password";
+
+            dt = ExDB.GetDataTable("AwesomeDB", ht, sql);
+
+            if (dt.Rows.Count > 0) 
+            {
+                txtReturn.Text = "Login Successful";
+            }
+            else
+            {
+                txtReturn.Text = "Login Failed";
+            }
+
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hashtable ht = new Hashtable();
+            string sql;
+            long lngReturn;
+
+            ht.Clear();
+
+            sql = "Insert into Users (Name, Username, Password, Email) Values (@Name, @Username, @Password, @Email)";
+            ht.Add("@Name", txtRName.Text);
+            ht.Add("@Username", txtRUsername.Text);
+            ht.Add("@Password", txtRPassword.Text);
+            ht.Add("@Email", txtREmail.Text);
+
+            lngReturn = ExDB.ExecuteIt("AwesomeDB", sql, ht);
+
+            txtRName.Text = "";
+            txtRUsername.Text = "";
+            txtRPassword.Text = "";
+            txtREmail.Text = "";
+        }
     }
 }
